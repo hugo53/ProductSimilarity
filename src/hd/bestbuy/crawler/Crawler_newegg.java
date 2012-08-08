bfd541ea14fdc1d217b97c7704882eda0f595874
duyvk <duyvk142@gmail.com>
2012-08-08 22:39:05 +0700

    crawler_newegg(all_link)

--- src/hd/bestbuy/crawler/Crawler_newegg.java
+++ src/hd/bestbuy/crawler/Crawler_newegg.java
@@ -1,23 +1,35 @@
 package hd.bestbuy.crawler;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.lang.model.type.PrimitiveType;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomText;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class Crawler {
-
+public class Crawler_newegg {
+	
+	public List<String> toCrawl = new ArrayList<String>(); 
+	public List<String> crawled = new ArrayList<String>();
+	public List<String> crawledItems = new ArrayList<String>();
+	
 	private WebClient client = null;
 	private HtmlPage page = null;
 	private String startUrl;
 
-	public Crawler() {
+	public Crawler_newegg() {
 		client = new WebClient(BrowserVersion.FIREFOX_3_6);
 		client.setJavaScriptEnabled(true);
 		client.setThrowExceptionOnScriptError(false);
@@ -25,6 +37,7 @@ public class Crawler {
 		client.setAjaxController(new NicelyResynchronizingAjaxController());
 		client.waitForBackgroundJavaScript(1000);
 		client.setCssEnabled(false);
+		
 	}
 
 	/**
@@ -41,7 +54,7 @@ public class Crawler {
 	public void setStartUrl(String startUrl) {
 		this.startUrl = startUrl;
 	}
-
+/*
 	public String getPropertyByXPath(HtmlPage page, String xPath) {
 		List<?> divs = page.getByXPath(xPath);
 		if (divs.size() > 0) {
@@ -62,28 +75,41 @@ public class Crawler {
 		return "NULL";
 	}
 
-	public void crawl(String url) throws FailingHttpStatusCodeException,
+*/	
+	private  boolean isSkipLink (String tmp){
+		//if( tmp.startsWith("/")||(!tmp.contains("newegg.com"))||(!tmp.startsWith("http")))
+			if(tmp.startsWith("http://www.newegg.com")
+					&& (tmp.contains("name=Laptops-Notebooks")
+					 ||tmp.contains("Item="))&&(!tmp.contains("ProductReview"))
+					)
+			  return false;
+			return true;
+		
+	}
+	
+	public List<String> get_all_link(String url) throws FailingHttpStatusCodeException,
 			MalformedURLException, IOException {
+		List<String> tmp = new ArrayList<String>();
 		try {
 			page = client.getPage(url);
 			//String originalHtml	= page.getWebResponse().getContentAsString();
-			String originalHtml = page.asText();
-			System.out.println(originalHtml);
+			//String originalHtml = page.asText();
+			//System.out.println(originalHtml);
+			
+			List<HtmlAnchor> anchors = page.getAnchors();
+			for (Iterator iter = anchors.iterator();iter.hasNext();){
+				HtmlAnchor anchor = (HtmlAnchor)iter.next();
+				
+				if(isSkipLink(anchor.getHrefAttribute())){
+					continue;
+				}
+				//System.out.println(anchor.getHrefAttribute());
+				//printWriter.println(anchor.getHrefAttribute());
+				tmp.add(anchor.getHrefAttribute());
+			}
+			
 			// GET INFORS
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+						
 			// get name
 			/*String name = getPropertyByXPath(page,
 					"//*[@id=\"productsummary\"]/h1/text()");
@@ -118,12 +144,45 @@ public class Crawler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	
+	return tmp;
 	}
 
-	public void crawlAllPages() {
-
+	public void crawl_web(List <String> seed) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		String tmp;
+		toCrawl.addAll(seed);
+		System.out.println(toCrawl.size());
+		while(!toCrawl.isEmpty()){
+			tmp = toCrawl.remove(toCrawl.size()-1);
+			System.out.println(toCrawl.size());
+			//System.out.println(tmp);
+			if(!crawled.contains(tmp)){
+				for(String newPage : get_all_link(tmp)){
+					if(newPage.contains("Item=")){
+						crawled.add(newPage);
+						crawledItems.add(newPage);
+						continue;
+					}
+					if(!toCrawl.contains(newPage))
+						toCrawl.add(newPage);
+				}
+				crawled.add(tmp);
+				System.out.println("ed" + crawled.size());
+			}
+			/*for(String newPage : get_all_link(tmp)){
+					crawled.add(newPage);
+			}
+			crawled.add(tmp);
+			System.out.println("ed" + crawled.size());*/
+		}
 	}
 
+	public void printPages(){
+		for (String p : crawledItems){
+			System.out.println(p);
+		}
+	}
+	
 	public void closeCrawler() {
 		client.closeAllWindows();
 	}
@@ -131,9 +190,14 @@ public class Crawler {
 	
 	  public static void main(String[] args) throws
 	  FailingHttpStatusCodeException, MalformedURLException, IOException {
-	  Crawler one = new Crawler(); one.crawl(
-	  "http://www.newegg.com/Product/Product.aspx?Item=N82E16834246856"
-	  ); one.closeCrawler(); }
+	  List <String> seed = new ArrayList<String>();
+	  seed.add("http://www.newegg.com/Store/SubCategory.aspx?SubCategory=32&name=Laptops-Notebooks");
+	  Crawler_newegg one = new Crawler_newegg(); 
+	  one.crawl_web(seed);
+	  one.printPages();
+	  one.closeCrawler(); 
+	  
+	  }
 	 
 }
 
